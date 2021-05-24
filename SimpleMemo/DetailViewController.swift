ce1855f5870adf24a7a21bab5a2524c20cc55085
32Moon <lmj970302@gmail.com>
2021-05-24 17:44:20 +0900

    ✨메모 공유 기능 구현

--- SimpleMemo/DetailViewController.swift
+++ SimpleMemo/DetailViewController.swift
@@ -11,12 +11,20 @@ class DetailViewController: UIViewController, UITextViewDelegate {
 
     var memo: Memo?
     var memoListViewModel = MemoViewModel()
+    @IBOutlet weak var doneButtonTrailing: NSLayoutConstraint!
     
     @IBOutlet weak var detailTextView: UITextView!
     @IBOutlet weak var doneButton: UIButton!
     
     @IBAction func backButtonClicked(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func shareButtonClicked(_ sender: Any) {
+        guard let memo = memo?.content else { return }
+        
+        let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
+        present(vc, animated: true, completion: nil)
     }
     
     @IBAction func doneButtonClicked(_ sender: Any) {
@@ -35,13 +43,13 @@ class DetailViewController: UIViewController, UITextViewDelegate {
         
         detailTextView.text = memo?.content
         detailTextView.delegate = self
-        self.doneButton.isHidden = true
+        self.doneButtonTrailing.constant = -40
         
         // Do any additional setup after loading the view.
     }
     
     func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
-        self.doneButton.isHidden = false
+        self.doneButtonTrailing.constant = 25
         return true
     }
     
